4ae778f4b7abb2f7ffb436602922045cc62174a4
adi-botak- <adityautamawijaya@gmail.com>
2016-03-10 12:35:36 +0800

    Assignment 3

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,7 +7,7 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
     nums_a.reverse!.each.with_index do |n, i|
     	unless i.even? && n != 0
-    		if n * 2 < 9
+    		unless n * 2 > 9
     			nums_a[i] = n * 2
     		else
     			nums_a[i] = n * 2 - 9
