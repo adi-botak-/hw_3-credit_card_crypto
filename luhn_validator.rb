d70427b936c6edb0d62f426a59f191d2e4c22641
adi-botak- <adityautamawijaya@gmail.com>
2016-03-10 11:21:57 +0800

    Assignment 3

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,15 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    nums_a.reverse!.each.with_index { |n, i| nums_a[i] = (n * 2) - (9 * (Math.log10(n * 2).to_i)) unless i.even? || n == 0 }
+    nums_a.reverse!.each.with_index do |n, i|
+    	unless i.even?
+    		unless n * 2 > 9
+    			nums_a[i] = n * 2
+    		else
+    			nums_a[i] = n * 2 - 9
+    		end
+    	end
+    end
     nums_a.reduce(&:+) % 10 == 0 ? true : false
     # TODO: use the integers in nums_a to validate its last check digit
   end
