7a06f25af0107e4ec2999c432c601d891a367f8b
adi-botak- <adityautamawijaya@gmail.com>
2016-03-13 21:07:51 +0800

    Revert "Assignment 4"
    This reverts commit 7e935df30db951889efaaa3f5b17dd7fd5a07ba7.

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,7 +7,6 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
-      key > 0 ? document.to_s.chars.map { |p| (p.ord + key).chr }.join : caesar_error
     end
 
     # Decrypts String document using integer key
@@ -17,11 +16,6 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
-      key > 0 ? document.to_s.chars.map { |c| (c.ord - key).chr }.join : caesar_error
-    end
-
-    def caesar_error
-    	"Key must be positive!"
     end
   end
 
@@ -33,12 +27,6 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
-      if key > 0
-      	permutation = (0..127).to_a.shuffle(random: Random.new(key))
-      	document.to_s.chars.map { |p| permutation[p.ord].chr }.join
-      else
-      	permutation_error
-      end
     end
 
     # Decrypts String document using integer key
@@ -48,16 +36,6 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
-      if key > 0
-      	permutation = (0..127).to_a.shuffle(random: Random.new(key))
-      	document.to_s.chars.map { |c| permutation.index(c.ord).chr }.join
-      else
-      	permutation_error
-      end
-    end
-
-    def permutation_error
-    	"Key must be positive!"
     end
   end
 end
