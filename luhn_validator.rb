c3741b39574802ed9a10d9d5893301321297205f
adi-botak- <adityautamawijaya@gmail.com>
2016-03-21 11:31:58 +0800

    Added the latest update

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,22 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = number.to_s.chars.map(&:to_i).reverse!
+    sum = 0
+    i = 0
+    nums_a.each do |n|
+      if i.even?
+        sum += n
+      else
+        if (n * 2 <= 9)
+          sum += n * 2
+        else
+          sum += n * 2 - 9
+        end
+      end
+      i += 1
+    end
+    sum % 10 == 0 ? true : false
   end
 end
--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,6 +7,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      if key > 0
+        document.to_s.chars.map { |d| (d.ord + key).chr }.join
+      else
+        caesar_error
+      end
     end
 
     # Decrypts String document using integer key
@@ -16,6 +21,15 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      if key > 0
+        document.to_s.chars.map { |d| (d.ord - key).chr }.join
+      else
+        caesar_error
+      end
+    end
+
+    def caesar_error
+      "Key must be positive!"
     end
   end
 
@@ -27,6 +41,12 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      if key > 0
+        lookup_table = (0..127).to_a.shuffle(random: Random.new(key))
+        document.to_s.chars.map { |d| lookup_table[d.ord].chr }.join
+      else
+        permutation_error
+      end
     end
 
     # Decrypts String document using integer key
@@ -36,6 +56,16 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      if key > 0
+        lookup_table = (0..127).to_a.shuffle(random: Random.new(key))
+        document.to_s.chars.map { |d| lookup_table.index(d.ord).chr }.join
+      else
+        permutation_error
+      end
+    end
+
+    def permutation_error
+      "Key must be positive!"
     end
   end
 end
