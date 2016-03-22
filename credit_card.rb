7875e2d4a797f18d21ac66620f62a0bc347547dd
adi-botak- <adityautamawijaya@gmail.com>
2016-03-22 20:32:16 +0800

    Applied rubocop

--- credit_card.rb
+++ credit_card.rb
@@ -23,7 +23,7 @@ class CreditCard
       expiration_date: @expiration_date,
       owner: @owner,
       credit_network: @credit_network
-     }.to_json
+    }.to_json
   end
 
   # returns all card information as single string
